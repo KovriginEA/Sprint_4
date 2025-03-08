c87dccc9ff5aa112b4b64b27f5d72ede433b2a0a
ЕВГЕНИЙ <kovriginea@yandex.ru>
2025-03-09 01:22:52 +0700

    first

--- tests.py
+++ tests.py
@@ -9,17 +9,41 @@ class TestBooksCollector:
     # дальше идет название метода, который тестируем add_new_book_
     # затем, что тестируем add_two_books - добавление двух книг
     def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
-
-        # добавляем две книги
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.get_books_genre()) == 2
 
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+    #проверка добавления книг с одинаковыми названиями
+    def test_add_new_book_two_same_books(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        assert len(collector.get_books_genre()) == 1
+
+    #проверка что у добавленной книги пустой жанр
+    def test_add_new_book_genry_is_empty(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        assert collector.get_book_genre('Гордость и предубеждение и зомби') == ''
+
+    #проверка установления жанра книги фантастика
+    def test_set_book_genre_fantsctic(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.set_book_genre('Гордость и предубеждение и зомби',collector.genre[0])
+        assert collector.get_book_genre('Гордость и предубеждение и зомби') == collector.genre[0]
+
+    #проверка что нельзя поставить жанр драма
+    def test_set_book_genre_dramma_is_not_allow(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.set_book_genre('Гордость и предубеждение и зомби', 'Драма')
+        assert collector.get_book_genre('Гордость и предубеждение и зомби') == ''
+
+    #проверка получения книг с жанром Комедии
+    def test_get_books_with_genry_comedy(self):
